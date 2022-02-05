d6324dc5ee4f2c77c6650c140289e7db9a32096d
manho <86037446+manho30@users.noreply.github.com>
2022-02-05 15:50:26 +0800

    update: added arab member detection.

--- functions/src/bot.js
+++ functions/src/bot.js
@@ -9,78 +9,82 @@ const bot = (body) => {
             }
         } else if (body.message.new_chat_member) {
             if (body.message.new_chat_member.id != bot_id) {
-                const qe1 = randomIntFromInterval(10, 150);
-                const qe2 = randomIntFromInterval(100, 150);
-                var answer = qe1 + qe2
-                var n1 = answer + 1;
-                var n2 = answer + 2;
-                var n3 = answer - 1;
-                var n4 = answer;
-                var n5 = answer - 2;
-                const buttons = [
-                    {
-                        "text": n1, 
-                        "callback_data": "re:false" + body.message.new_chat_member.id
-                    }, 
-                    {
-                        "text": n2, 
-                        "callback_data": "re:false" + body.message.new_chat_member.id
-                    }, 
-                    {
-                        "text": n3, 
-                        "callback_data": "re:false" + body.message.new_chat_member.id
-                    }, 
-                    {
-                        "text": n4, 
-                        "callback_data": "re:true" + body.message.new_chat_member.id
-                    }, 
-                    {
-                        "text": n5, 
-                        "callback_data": "re:false" + body.message.new_chat_member.id
-                    },
-                    {
-                        "text": "封禁(管理员)", 
-                        "callback_data": "re:ban" + body.message.new_chat_member.id
-                    }, 
-                    
-                ] 
-                return [mute(body.message.chat.id, body.message.new_chat_member.id,0), sendMessage(body.message.chat.id, getMentionName(body.message.new_chat_member) + " 欢迎加入本群，" +
-                    "请根据提示完成人机验证\n\n" + qe1 + " + " + qe2 + " = ❓❓\n\n管理员点击任意按钮新成员可通过。", generateInlineKeyboardMarkup(buttons, 5)), deleteMessage(body.message.chat.id, body.message.message_id)] 
+                if (!nameIsArabic(body.message.new_chat_member)) {
+                    const qe1 = randomIntFromInterval(10, 150);
+                    const qe2 = randomIntFromInterval(100, 150);
+                    var answer = qe1 + qe2
+                    var n1 = answer + 1;
+                    var n2 = answer + 2;
+                    var n3 = answer - 1;
+                    var n4 = answer;
+                    var n5 = answer - 2;
+                    const buttons = [{
+                            "text": n1,
+                            "callback_data": "re:false" + body.message.new_chat_member.id
+                        },
+                        {
+                            "text": n2,
+                            "callback_data": "re:false" + body.message.new_chat_member.id
+                        },
+                        {
+                            "text": n3,
+                            "callback_data": "re:false" + body.message.new_chat_member.id
+                        },
+                        {
+                            "text": n4,
+                            "callback_data": "re:true" + body.message.new_chat_member.id
+                        },
+                        {
+                            "text": n5,
+                            "callback_data": "re:false" + body.message.new_chat_member.id
+                        },
+                        {
+                            "text": "封禁(管理员)",
+                            "callback_data": "re:ban" + body.message.new_chat_member.id
+                        },
+
+                    ]
+                    return [mute(body.message.chat.id, body.message.new_chat_member.id, 0), sendMessage(body.message.chat.id, getMentionName(body.message.new_chat_member) + " 欢迎加入本群，" +
+                        "请根据提示完成人机验证\n\n" + qe1 + " + " + qe2 + " = ❓❓\n\n管理员点击任意按钮新成员可通过。", generateInlineKeyboardMarkup(buttons, 5)), deleteMessage(body.message.chat.id, body.message.message_id)]
+                } else {
+                    return [kick(body.message.chat.id, body.message.new_chat_member.id), sendMessage(body.message.chat.id, `本群群员无法理解阿拉伯语，已清除。${getMentionName(body.message.new_chat_member)} \n（耶，又干走了一个小清真！）}`)]
+                }
             } else {
                 return sendMessage(body.message.chat.id, "入群验证服务已激活\n群id：" + body.message.chat.id)
             }
-        } else if (body.message.left_chat_member){
-            return deleteMessage(body.message.chat.id, body.message.message_id) 
-        } 
-    } else if(body.callback_query){
-        if(body.callback_query.data.indexOf("re") >= 0){
+
+        } else if (body.message.left_chat_member) {
+            return deleteMessage(body.message.chat.id, body.message.message_id)
+        }
+    } else if (body.callback_query) {
+        if (body.callback_query.data.indexOf("re") >= 0) {
             const data = body.callback_query.data
-            if(data.indexOf("re:false") >= 0){
-                if(body.callback_query.from.id != data.replace("re:false", "")){
-                    if(!isAdmin(body.callback_query.from.id, body.callback_query.message.chat.id)){
-                        return answerCallBackQuery(body.callback_query.id, "又不是问你，自作多情") 
+            if (data.indexOf("re:false") >= 0) {
+                if (body.callback_query.from.id != data.replace("re:false", "")) {
+                    if (!isAdmin(body.callback_query.from.id, body.callback_query.message.chat.id)) {
+                        return answerCallBackQuery(body.callback_query.id, "又不是问你，自作多情")
                     } else {
-                        return [editMessage(body.callback_query.message.chat.id,body.callback_query.message.message_id,"✅✅管理员通过验证"), unmute(body.callback_query.message.chat.id, data.replace("re:false", ""))] 
+                        return [editMessage(body.callback_query.message.chat.id, body.callback_query.message.message_id, "✅✅管理员通过验证"), unmute(body.callback_query.message.chat.id, data.replace("re:false", ""))]
                     }
                 } else {
                     return [editMessage(body.callback_query.message.chat.id, body.callback_query.message.message_id, "❌❌回答错误"), kick(body.callback_query.message.chat.id, data.replace("re:false", ""))]
                 }
-            } else if(data.indexOf("re:true") >= 0) {
-                if(body.callback_query.from.id != data.replace("re:true", "")){                 
-                    if(!isAdmin(body.callback_query.from.id, body.callback_query.message.chat.id)){
+            } else if (data.indexOf("re:true") >= 0) {
+                if (body.callback_query.from.id != data.replace("re:true", "")) {
+                    if (!isAdmin(body.callback_query.from.id, body.callback_query.message.chat.id)) {
                         return answerCallBackQuery(body.callback_query.id, "请无关人士不要来捣乱")
                     } else {
-                        return [editMessage(body.callback_query.message.chat.id,body.callback_query.message.message_id,"✅✅管理员通过验证"), unmute(body.callback_query.message.chat.id, data.replace("re:true", ""))] 
+                        return [editMessage(body.callback_query.message.chat.id, body.callback_query.message.message_id, "✅✅管理员通过验证"), unmute(body.callback_query.message.chat.id, data.replace("re:true", ""))]
                     }
                 } else {
                     return [unmute(body.callback_query.message.chat.id, data.replace("re:true", "")), editMessage(body.callback_query.message.chat.id, body.callback_query.message.message_id, "✅✅回答正确")]
                 }
-            } else if(data.indexOf("re:ban") >= 0){
-                if(isAdmin(body.callback_query.from.id, body.callback_query.message.chat.id)){
-                    return [kick(body.callback_query.message.chat.id, data.replace("re:ban", "")), editMessage(body.callback_query.message.chat.id, body.callback_query.message.message_id, "⭕⭕管理员手动封禁成功")] 
+            } else if (data.indexOf("re:ban") >= 0) {
+                if (isAdmin(body.callback_query.from.id, body.callback_query.message.chat.id)) {
+                    return [kick(body.callback_query.message.chat.id, data.replace("re:ban", "")), editMessage(body.callback_query.message.chat.id, body.callback_query.message.message_id, "⭕⭕管理员手动封禁成功")]
                 } else {
                     return answerCallBackQuery(body.callback_query.id, "你是管理员？")
-                } 
+                }
             }
         }
     }
