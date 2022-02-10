51ebf15cac5d7bb23395640b9ebbbea389d2c81a
manho <86037446+manho30@users.noreply.github.com>
2022-02-10 23:17:49 +0800

    update: added note

--- functions/src/fetch.js
+++ functions/src/fetch.js
@@ -1,3 +1,8 @@
+/**                  created by @manho MIT License 2022. 
+                     Support in Google App Script 
+**/
+
+// fetch 
 const fetch = (link) =>	{
     return UrlFetchApp.fetch(link)
 } 
@@ -13,13 +18,13 @@ const deletewebhook = () => {
     const res = fetch("api.telegram.org/bot"+token+"/deletewebhook")
     Logger.log(res)
 }
-
-var testList2 = '{"update_id":427275035,"message":{"message_id":211,"from":{"id":539065210,"is_bot":false,"first_name":"Yufeng Deng","last_name":"峰哥","username":"fennng","language_code":"en-US"},"chat":{"id":"1381836444","first_name":"Yufeng Deng","last_name":"峰哥","username":"fennng","type":"private"},"date":1540807486,"text":"/start","entities":[{"offset":0,"length":5,"type":"bot_command"}]}}';
-var isDebug = false;
+// debug 
+//var testList2 = '{"update_id":427275035,"message":{"message_id":211,"from":{"id":539065210,"is_bot":false,"first_name":"Yufeng Deng","last_name":"峰哥","username":"fennng","language_code":"en-US"},"chat":{"id":"1381836444","first_name":"Yufeng Deng","last_name":"峰哥","username":"fennng","type":"private"},"date":1540807486,"text":"/start","entities":[{"offset":0,"length":5,"type":"bot_command"}]}}';
+//var isDebug = false;
  
-function debug() {
-  e = {};
-  e.postData = {};
-  e.postData.contents = testList2;
-  doPost(e);
-}
+//function debug() {
+//  e = {};
+//  e.postData = {};
+//  e.postData.contents = testList2;
+//  doPost(e);
+//}
